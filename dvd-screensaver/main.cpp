ef4efbdf2e2d1d78aca3ea3bf9f56c69f8480f4b
Bence Deli <delibnc@gmail.com>
2026-02-06 23:29:15 +0100

    performance improvements, remove WM_TIMER, implement delta time,

--- dvd-screensaver/main.cpp
+++ dvd-screensaver/main.cpp
@@ -8,6 +8,9 @@
 #include <gdiplus.h>
 #include <Shlwapi.h>
 #include <objidl.h>
+#include <chrono>
+#include <cmath>
+
 //LIBRARIES
 #pragma comment(lib, "ScrnSavw.lib")
 #pragma comment(lib, "opengl32.lib")
@@ -16,8 +19,10 @@
 #pragma comment (lib,"Gdiplus.lib")
 #pragma comment(lib, "dwmapi.lib")
 #pragma comment(lib, "Shlwapi.lib")
+#pragma comment(lib, "winmm.lib")
 
 #define TIMER 1
+
 
 void InitGL(HWND hWnd, HDC& hDC, HGLRC& hRC);
 void CloseGL(HWND hWnd, HDC hDC, HGLRC hRC);
@@ -28,10 +33,68 @@ void OnTimer(HDC hDC);
 int Width, Height; //globals for size of screen
 float Aspect;
 float camL, camR, camB, camT;
+
+// fps
+static DWORD rec_time;
+static DWORD prev_time;
+static int count_frame;
+static int count_fps;
+std::chrono::steady_clock::time_point stc_prev;
+
+//settings
+
+static int waitValue = 15;
+
+void dbgprint(float v) {
+    wchar_t text_buffer[20] = { 0 }; //temporary buffer
+    swprintf(text_buffer, _countof(text_buffer), L"%f\n", v); // convert
+    OutputDebugString(text_buffer); // print
+}
+
+
+
 //////////////////////////////////////////////////
 ////   INFRASTRUCTURE -- THE THREE FUNCTIONS   ///
 //////////////////////////////////////////////////
+static void initCountFps(void);
+static void closeCountFps(void);
+static void countFps(void);
+// ========================================
+// Init count FPS work
+static void initCountFps(void)
+{
+    timeBeginPeriod(1);
+    rec_time = timeGetTime();
+    prev_time = rec_time;
+    count_fps = 0;
+    count_frame = 0;
+}
 
+// Close count FPS
+static void closeCountFps(void)
+{
+    timeEndPeriod(1);
+}
+
+// count FPS
+static void countFps(void)
+{
+    count_frame++;
+    DWORD t = timeGetTime() - rec_time;
+
+    if (t >= 1000)
+    {
+        rec_time += 1000;
+        count_fps = count_frame;
+        count_frame = 0;
+    }
+    else if (t < 0)
+    {
+        rec_time = timeGetTime();
+        count_fps = 0;
+        count_frame = 0;
+    }
+}
 
 // Screen Saver Procedure
 LRESULT WINAPI ScreenSaverProc(HWND hWnd, UINT message,
@@ -40,7 +103,7 @@ LRESULT WINAPI ScreenSaverProc(HWND hWnd, UINT message,
     static HDC hDC;
     static HGLRC hRC;
     static RECT rect;
-
+    SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
     //FreeConsole();
 
     switch (message) {
@@ -52,23 +115,25 @@ LRESULT WINAPI ScreenSaverProc(HWND hWnd, UINT message,
         Width = rect.right;
         Height = rect.bottom;
         Aspect = (Width + 0.0f) / Height;
-
+        initCountFps();         // initialize FPS counter
         // setup OpenGL, then animation
         InitGL(hWnd, hDC, hRC);
         SetupAnimation(Width, Height);
 
         //set timer to tick every 10 ms
-        SetTimer(hWnd, TIMER, 2, NULL);
+        //SetTimer(hWnd, TIMER, waitValue, NULL);
         return 0;
 
     case WM_DESTROY:
+        closeCountFps();
         KillTimer(hWnd, TIMER);
         CleanupAnimation();
         CloseGL(hWnd, hDC, hRC);
         return 0;
 
-    case WM_TIMER:
-        OnTimer(hDC);       //animate!      
+    case WM_PAINT:
+        OnTimer(hDC);       //animate!     
+        InvalidateRect(hWnd, NULL, FALSE);
         return 0;
 
     }
@@ -115,36 +180,40 @@ const GLfloat COLORS[NCOLOR][3] = {
 };
 
 struct Item {
-    GLfloat X = 200, Y = 0, Step = 1;
-    GLfloat Width = 100, Height = 100;
-    GLfloat XDir = 1, YDir = 1;
+    float X = 200, Y = 0, speed = 60;
+    float Width = 100, Height = 100;
+    float XDir = 1, YDir = 1;
     int ColorCounter = -1;
-
-    void Move() {
+    
+    void Move(float delta) {
         int corner = 0;
-        if ((int)X < camL + Width / 2 || X + Width / 2 >(int)camR) {
+
+        float dX = XDir * speed * delta;
+        float dY = YDir * speed * delta;
+        bool hitX = false, hitY = false;
+
+        //X check
+        X += dX;
+        if (X - Width / 2 < camL || X + Width / 2 > camR) {
             XDir *= -1;
-            Collision();
-            corner++;
+            X += XDir * fabs(dX); // bounce remainder
+            hitX = true;
         }
-        //OK
-        if (Y == camB + Height / 2 || Y + Height / 2 == camT) {
+        //Y check
+        Y += dY;
+        if (Y - Height / 2 < camB || Y + Height / 2 > camT) {
             YDir *= -1;
-            Collision();
-            corner++;
+            Y += YDir * fabs(dY);
+            hitY = true;
         }
 
-        if (corner == 2) {
-            ColorCounter -= 2;
-            glColor3f(246.0/255, 162.0/255, 23.0/255); //specific color when hits corner
+        if (hitX && hitY) {
+            glColor3f(246.0 / 255, 162.0 / 255, 23.0 / 255); //specific color when hits corner
         }
-        X += XDir * Step;
-        Y += YDir * Step;
-    }
-
-    void Collision() {
-        if (++ColorCounter == NCOLOR) ColorCounter = 0;
-        glColor3fv(COLORS[ColorCounter]);
+        else if (hitX || hitY) {
+            if (++ColorCounter == NCOLOR) ColorCounter = 0;
+            glColor3fv(COLORS[ColorCounter]);
+        }
 
     }
 };
@@ -205,13 +274,27 @@ void loadImage() {
 static void InitGL(HWND hWnd, HDC& hDC, HGLRC& hRC)
 {
 
-    PIXELFORMATDESCRIPTOR pfd;
-    ZeroMemory(&pfd, sizeof pfd);
-    pfd.nSize = sizeof pfd;
-    pfd.nVersion = 1;
-    pfd.dwFlags = PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-    pfd.iPixelType = PFD_TYPE_RGBA;
-    pfd.cColorBits = 24;
+    PIXELFORMATDESCRIPTOR pfd =
+    {
+        sizeof(PIXELFORMATDESCRIPTOR), // size of this pfd
+        1,                             // version number
+        // support window, OpenGL, double bufferd
+        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+        PFD_TYPE_RGBA,  // RGBA type
+        32,             // color
+        0, 0,           // R
+        0, 0,           // G
+        0, 0,           // B
+        0, 0,           // A
+        0,              // accumulation buffer
+        0, 0, 0, 0,     // accum bits ignored
+        24,             // depth
+        8,              // stencil buffer
+        0,              // auxiliary buffer
+        PFD_MAIN_PLANE, // main layer
+        0,              // reserved
+        0, 0, 0         // layermask, visiblemask, damagemask
+    };
 
     hDC = GetDC(hWnd);
 
@@ -235,24 +318,23 @@ static void CloseGL(HWND hWnd, HDC hDC, HGLRC hRC)
 
 void SetupAnimation(int Width, int Height)
 {
-    //window resizing stuff
-    glViewport(0, 0, (GLsizei)Width, (GLsizei)Height);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-
-    float unit = Width/10;
+    float unit = Width / 10;
     camL = (-unit * Aspect);
     camR = -camL;
     camB = -unit;
     camT = -camB;
 
+    //window resizing stuff
+    glViewport(0, 0, (GLsizei)Width, (GLsizei)Height);
 
-    glOrtho(camL, camR, camB, camT, 25.0, 75.0);
-    glMatrixMode(GL_MODELVIEW);
-
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluLookAt(0.0, 0.0, 50.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glOrtho(camL, camR, camB, camT, -1, 1);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    //gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     //camera xyz, the xyz to look at, and the up vector (+y is up)
 
     //background
@@ -261,7 +343,8 @@ void SetupAnimation(int Width, int Height)
     glColor3f(1.0, 1.0, 1.0); //white
 
     loadImage();
-
+    //prev_time = timeGetTime();
+    stc_prev = std::chrono::steady_clock::now();
 }
 
 //static GLfloat spin = 0;   //a global to keep track of the square's spinning
@@ -273,14 +356,23 @@ static float yvals[] = { i.Height / 2, -i.Height / 2, -i.Height / 2, i.Height / 
 
 void OnTimer(HDC hDC) //increment and display
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    std::chrono::steady_clock::time_point stc_now = std::chrono::steady_clock::now();
+    std::chrono::duration<double> delt = std::chrono::duration_cast<std::chrono::duration<double>>(stc_now - stc_prev);
+    stc_prev = stc_now;
+
+
+    i.Move(delt.count());
+
+    glClearColor(0, 0, 0, 0.15f);
+    glClear(GL_COLOR_BUFFER_BIT);
     glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glPushMatrix();
-
-    i.Move();
-
+    
     glTranslatef(i.X, i.Y, 0);
-
+    
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glBegin(GL_POLYGON);
     for (int j = 0; j < 4; j++) {
@@ -289,12 +381,21 @@ void OnTimer(HDC hDC) //increment and display
     } 
     glEnd();
 
-    //glPopMatrix();
-
-    glFlush();
-    SwapBuffers(hDC);
     glPopMatrix();
     glDisable(GL_TEXTURE_2D);
+    //glFlush();
+    
+    //glColor4f(1, 1, 1, 1);           // set color
+    /*glRasterPos3f(-1, 1.0, -1.08); // set postion
+    {
+        char buf[512];
+        sprintf_s(buf, "%d FPS", count_fps);
+        drawText(buf);
+    }
+    */
+    SwapBuffers(hDC);
+
+    countFps(); // calc FPS
 }
 
 void CleanupAnimation()
